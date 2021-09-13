89f7593e610012e0b3cc098376276eee72487492
zly <1371935318@qq.com>
2021-09-14 00:14:49 +0800

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,88 +1,95 @@
-import axios from 'axios'
+import axios from "axios";
 
-import { Toast } from 'antd-mobile';
+import { Toast } from "antd-mobile";
 
-axios.defaults.withCredentials=true;
+axios.defaults.withCredentials = true;
 
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000*200, //1m request timeout
-  headers:{
-    platform:'juooo-zly'
+  timeout: 5000 * 200, //1m request timeout
+  headers: {
+    platform: "film-ticket-sale-zly",
   },
   withCredentials: true,
-  crossDomain:true 
-  
+  crossDomain: true,
 });
 
-export default service
-service.interceptors.request.use(config => {
-  // config.headers['token'] = getToken()
-  return config
-}, error => {
-  Promise.reject(error)
-});
-
+export default service;
+service.interceptors.request.use(
+  (config) => {
+    // config.headers['token'] = getToken()
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 service.interceptors.response.use(
-  response => {
-
-    if(response.data.error===403){
+  (response) => {
+    if (response.data.error === 403) {
       // removeToken();
-      // router.currentRoute.path!='/login'?router.replace({path:"/login",query:{redirect:router.currentRoute.fullPath}}):null;//去登录; 
-      response.data.data={};
+      // router.currentRoute.path!='/login'?router.replace({path:"/login",query:{redirect:router.currentRoute.fullPath}}):null;//去登录;
+      response.data.data = {};
       return response;
-    }else{
+    } else {
       return response;
-    }   
-
+    }
   },
-  error => {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
 
-
-
-let api = "";//跨域配置的api
-if(process.env.NODE_ENV === "development") {
-    api = "/api"
+let api = ""; //跨域配置的api
+if (process.env.NODE_ENV === "development") {
+  api = "/api";
 }
 
-
-
-export function post(url,data,{isLoading=false,text="加载中..."}={}){
-  return new Promise((resolve,reject)=>{
-    if(isLoading) Toast.loading(text, 0);
+export function post(
+  url,
+  data,
+  { isLoading = false, text = "加载中..." } = {}
+) {
+  return new Promise((resolve, reject) => {
+    if (isLoading) Toast.loading(text, 0);
     service({
       url: api + url,
-      method:'POST',
-      data:data,
-      headers:{}
-    }).then((res)=>{
-      resolve(res.data);
-      if(isLoading) Toast.hide();
-    }).catch(err=>{
-      reject(err);
-      Toast.fail(err.message, 2)
+      method: "POST",
+      data: data,
+      headers: {},
     })
-  })
+      .then((res) => {
+        resolve(res.data);
+        if (isLoading) Toast.hide();
+      })
+      .catch((err) => {
+        reject(err);
+        Toast.fail(err.message, 2);
+      });
+  });
 }
 
-export function get(url,params,{isLoading=false,text="加载中..."}={}){
-  return new Promise((resolve,reject)=>{
-    if(isLoading) Toast.loading(text, 0);
+export function get(
+  url,
+  params,
+  { isLoading = false, text = "加载中..." } = {}
+) {
+  return new Promise((resolve, reject) => {
+    if (isLoading) Toast.loading(text, 0);
     service({
-      url:api + url,
-      method:'GET',
-      params:params,
-      headers:{}      
-    }).then((res)=>{
-      resolve(res.data);
-      if(isLoading) Toast.hide();
-    }).catch(err=>{
-      reject(err);
-      Toast.fail(err.message, 2);
+      url: api + url,
+      method: "GET",
+      params: params,
+      headers: {},
     })
-  })
+      .then((res) => {
+        resolve(res.data);
+        if (isLoading) Toast.hide();
+      })
+      .catch((err) => {
+        reject(err);
+        Toast.fail(err.message, 2);
+      });
+  });
 }
