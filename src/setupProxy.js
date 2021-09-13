0d72a02f53e5713fbcfe7c42916201dc54b4e654
zly <1371935318@qq.com>
2021-09-14 00:09:40 +0800

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use('/api', createProxyMiddleware({ 
-        target: 'http://192.168.0.111:7002',
+        target: 'http://localhost:7002',
         changeOrigin: true,
         pathRewrite: {
             "^/api": "/"
--- src/view/main/Home/index.js
+++ src/view/main/Home/index.js
@@ -1,33 +1,48 @@
+import React, { Component } from "react";
+import "./index.scss";
 
+import { get_film_hot } from "../../../api/film";
+import { Button, WhiteSpace } from "antd-mobile";
 
-import React, { Component } from 'react'
-import './index.scss'
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-import { get_film_hot } from '../../../api/film';
+  componentDidMount() {
+    get_film_hot({}).then((res) => {
+      console.log("666", res);
+    });
+  }
 
-
-class Home extends Component{
-	constructor(props){
-		super(props)
-		this.state = {
-			
-		}
-	}
-
-	componentDidMount(){
-		get_film_hot({}).then(res=>{
-			console.log('666',res);
-		})
-	}
-	
-	render(){
-		return (
-			<div className='app-home'>
-				app-home
-			</div>
-		)
-	}
-	
+  render() {
+    return (
+      <div className="app-home">
+        app-home
+        <Button>default</Button>
+        <WhiteSpace />
+        <Button disabled>default disabled</Button>
+        <WhiteSpace />
+        <Button type="primary">primary</Button>
+        <WhiteSpace />
+        <Button type="primary" disabled>
+          primary disabled
+        </Button>
+        <WhiteSpace />
+        <Button type="warning">warning</Button>
+        <WhiteSpace />
+        <Button type="warning" disabled>
+          warning disabled
+        </Button>
+        <WhiteSpace />
+        <Button loading>loading button</Button>
+        <WhiteSpace />
+        <Button icon="check-circle-o">with icon</Button>
+        <WhiteSpace />
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
